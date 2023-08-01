49d91e99719939d7cb7443bb845720a6d3bf9ec6
Farel Winalda <135517117+FarelW@users.noreply.github.com>
2023-08-01 14:26:24 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState,useEffect } from "react";
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        HALOOO
+      </div>
+    </Router>
+  );
+}
+
+export default App;

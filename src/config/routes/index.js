0caea1699b7517e6ad5a05ebfc6dab7aa1c7faf2
Farel Winalda <eeksapi321@gmail.com>
2023-08-06 13:37:21 +0700

    Update index.js

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./../../pages/home/Home";
 import { Admin, Reservation } from "../../pages";
+import NavBar from './../../components/navbar/NavBar';
 
 const Routess = () => {
     return (
         <Router>
+            <NavBar/>
             <Routes>
                 <Route path="/reservation" element={<Reservation />} />
                 <Route path="/admin" element={<Admin />} />

23152483a0285fbf1a1d17d1c618690fdb5af8c6
rafi-sama <rafimaliki28@gmail.com>
2023-08-07 10:12:59 +0700

    qr (reactnya blm kelar)

---
+++ src/pages/qr/prev-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function prev_page() {
+  return (
+    <div>
+      <a href="qrcode-enter.html">
+        <button className="book-button">Book</button>
+      </a>
+    </div>
+  );
+}
+
+export default prev_page;
